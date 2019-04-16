e89d4a687cd51dcbbb04961c8354e19ecd5eab26
github username <github email address>
2019-04-16 09:41:43 -0500

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,19 +1,19 @@
 const expect = chai.expect;
-describe('Fix the Scope', function() {
-  describe('declare customerName to be bob in global scope', function() {
+describe('Fix the Scope', function () {
+  describe('declare customerName to be bob in global scope', function () {
     it('returns the customerName', () => {
       expect(window.customerName).to.equal('bob')
     })
   })
 
-  describe('upperCaseCustomerName()', function() {
+  describe('upperCaseCustomerName()', function () {
     it('returns the customerName', () => {
       expect(window.customerName).to.equal('bob')
       upperCaseCustomerName()
       expect(window.customerName).to.equal('BOB')
     })
   })
-  describe('setBestCustomer()', function() {
+  describe('setBestCustomer()', function () {
     it('setBestCustomer', () => {
       expect(window.bestCustomer).to.equal(undefined)
       window.setBestCustomer()
@@ -21,20 +21,20 @@ describe('Fix the Scope', function() {
     })
   })
 
-  describe('overWriteBestCustomer()', function() {
+  describe('overWriteBestCustomer()', function () {
     it('overwrites the best customer', () => {
       overWriteBestCustomer('maybe bob')
       expect(window.bestCustomer).to.equal('maybe bob')
     })
   })
 
-  describe('reassignLeastFavorite', function(){
+  describe('reassignLeastFavorite', function () {
     it('unsuccessfully tries to reassign the least favorite customer', () => {
       expect(changeLeastFavoriteCustomer).to.throw(TypeError)
     })
   })
 
-  describe('attemptTwoFavoriteCustomers', function(){
+  describe('attemptTwoFavoriteCustomers', function () {
     it('unsuccessfully tries to declare favoriteCustomer with let two times', () => {
       expect(attemptTwoFavoriteCustomers).to.throw(SyntaxError)
     })
